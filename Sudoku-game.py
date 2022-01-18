9b0e6641cb35792ed1806fda02049ff84577d502
Liam Neate <74662740+LiamNeate@users.noreply.github.com>
2022-01-18 22:24:39 +0000

    Done the creation of the random sudoku board
    Using the backtracking algorithm provided by 101computing.net, I have created a random board generator. Mine uses the main functionality of the backtracking, the filling in the board, and applies it to an empty board. To do this I had to first set the board to empty to begin with. I quickly realised that they use a linear way of checking each number so the boards will always look the same. I added in random numbers that randomly sets the order of the numbers that are going to be checked. This creates a unique board each time. I also had to learn how to use copy.deepcopy as I had to make a copy of the original blank board so that I could easily set the current grid back to it, if the program creates a board that doesn't work.
    
    Next I had to remove random numbers from the board that the user will have to fill in. I had to use my prior research of the minimum amount of numbers that have to be there in order for it to be solvable. Now I just had to use random numbers to remove numbers from the grid. I made another copy of the grid and used this to initially remove the numbers. I would remove random numbers from this temporary grid and then run it through the solver to see if it was still solvable. If it is solvable, then I would set the main grid as the same as the temporary grid. If it wasn't solvable, it would run the function again with a different number.
    
    It then prints out the final grid to show the user.
    
    Next I need to convert this into C++.

---
+++ Sudoku-game.py
@@ -0,0 +1,111 @@
+#Sudoku Random Game Generator - Liam Neate - With code from Backtracking Algorithm - Sudoku Solver - www.101computing.net/backtracking-algorithm-sudoku-solver/
+from random import shuffle
+from random import randint
+from copy import deepcopy
+#importing random to enter random numbers as solutions
+
+#initialise empty 9 by 9 grid
+grid = []
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+#Using deepcopy to create a copy that won't be changed once the other is
+gridOG = deepcopy(grid)
+
+#A function to check if the grid is full
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+    #Simple check using for and if's to check none of the squares equal 0
+
+  #We have a complete grid!  
+  return True 
+
+#A backtracking/recursive function to check all possible combinations of numbers until a
+#solution is found
+def solveGrid(grid):
+  #Find next empty cell
+  for i in range(0,81):
+    possibles = list(range(1,10))
+    shuffle(possibles)
+    row=i//9
+    col=i%9
+    #These are used to assure that each grid is checked.
+    if grid[row][col]==0:
+    #If found an empty square
+      for value in possibles:
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #print("Check")
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                return True
+              else:
+                if solveGrid(grid):
+                  return True
+      break
+
+def removeNumbers(grid, tempGrid):
+    for i in range (0, randint(54, 64)):
+        randVal = randint(0,80)
+        randRow = randVal //9
+        randCol = randVal % 9
+        #Setting tempGrid to the current grid so it can make sure that it works with all
+        #the numbers specified missing
+        tempGrid = deepcopy(grid)
+        tempGrid[randRow][randCol] = 0
+        if (solveGrid(tempGrid) == True):
+            grid[randRow][randCol]=0
+        else:
+            pass
+    return True
+
+#Doing the first grid create
+solved = solveGrid(grid)
+#Checking if it worked, if not, set grid back to 0's and try again until it works
+while solved != True:
+    grid = deepcopy(gridOG)
+    solved = solveGrid(grid)
+print("New grid generated!")
+#Creating another copy of the grid so one can be tested to see 
+tempGrid = deepcopy(grid)
+removeNumbers(grid, tempGrid)
+for x in grid:
+    print(x)
+
+#Change the solve grid to be used to see if a number that is passed through can work.
